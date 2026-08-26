3c91f0c9899c342da37ab521f8c57dac7a9475f9
eas4ai <shawn.payments@gmail.com>
2026-08-26 14:35:36 -0400

    chore: support Suprnova v1.3.6

--- tests/public_sdk_firewall.rs
+++ tests/public_sdk_firewall.rs
@@ -23,7 +23,7 @@ fn manifest_depends_on_the_public_tag_without_magnetar_or_paths() {
     );
     assert_eq!(
         suprnova.get("tag").and_then(toml::Value::as_str),
-        Some("v1.3.5")
+        Some("v1.3.6")
     );
     assert!(!suprnova.contains_key("path"));
 }
